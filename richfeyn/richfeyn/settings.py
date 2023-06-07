16ca502e96b2a38ef52dfb8a1faf72572d3b0a8d
Anshpreet123 <108647469+Anshpreet123@users.noreply.github.com>
2023-06-07 15:34:13 +0530

    settings changed for deployment

--- richfeyn/richfeyn/settings.py
+++ richfeyn/richfeyn/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-o6of)v9mjn2hfc#^z_(8u8%yas+9p_6nww)=39@g4pzj84!4^h'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
